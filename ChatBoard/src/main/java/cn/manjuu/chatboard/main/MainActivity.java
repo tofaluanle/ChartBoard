563d4022b284c44b7570197501fa530180ffc9e3
sjj <songjiangjiang@sina.com>
2016-02-03 10:56:27 +0800

    改为使用PopupWindow实现菜单了

--- ChatBoard/src/main/java/cn/manjuu/chatboard/main/MainActivity.java
+++ ChatBoard/src/main/java/cn/manjuu/chatboard/main/MainActivity.java
@@ -1,8 +1,7 @@
 package cn.manjuu.chatboard.main;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
@@ -13,9 +12,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,7 +25,7 @@ import android.widget.TextView;
 import cn.manjuu.chatboard.R;
 import cn.manjuu.chatboard.setting.SettingActivity;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private DrawerLayout drawer;
     int index;
@@ -37,6 +36,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     TabLayout tabLayout;
     MyFragmentAdapter mFragmentAdapter;
     Toolbar toolbar;
+    PopupWindow mPopupWindow;
+    boolean isShow;
+    View btn_menu;
+    ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,20 +47,10 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                mAdapter.addData("haha" + index++, 1);
-                mFragmentAdapter.notifyDataSetChanged();
-            }
-        });
-
-        findViewById(R.id.btn_return).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                mAdapter.removeData(1);
-            }
-        });
+        findViewById(R.id.add).setOnClickListener(this);
+        findViewById(R.id.btn_return).setOnClickListener(this);
+        btn_menu = findViewById(R.id.btn_menu);
+        btn_menu.setOnClickListener(this);
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -73,7 +66,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private void initTabAndPager() {
         tabLayout = (TabLayout) findViewById(R.id.id_tablayout);
-        ViewPager mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
         mFragmentAdapter = new MyFragmentAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(mFragmentAdapter);
         tabLayout.setupWithViewPager(mViewPager);
@@ -109,8 +102,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case KeyEvent.KEYCODE_DPAD_RIGHT:
                 drawer.openDrawer(GravityCompat.START);
                 break;
+            case KeyEvent.KEYCODE_MENU:
+                if (isShow) {
+                    isShow = false;
+                }
+                return true;
         }
         return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_MENU:
+                if (!isShow) {
+                    showPopupWindow();
+                } else {
+                    isShow = false;
+                }
+                return true;
+        }
+        return super.onKeyUp(keyCode, event);
     }
 
     @Override
@@ -122,33 +134,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-//        menu.add("menu");// 必须创建一项
-        getMenuInflater().inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-//    @Override
-//    public boolean onMenuOpened(int featureId, Menu menu) {
-//        if (mPopupWindow != null) {
-//            if (!mPopupWindow.isShowing()) {
-//            /*最重要的一步：弹出显示   在指定的位置(parent)  最后两个参数 是相对于 x / y 轴的坐标*/
-//                mPopupWindow.showAtLocation(toolbar, Gravity.BOTTOM, 0, 0);
-//            }
-//        }
-//        return false;// 返回为true 则显示系统menu
-//    }
-    PopupWindow mPopupWindow;
-
     private void initPopupWindow() {
         LayoutInflater mLayoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-    /*设置显示menu布局   view子VIEW*/
         View sub_view = mLayoutInflater.inflate(R.layout.main_menu_ctg, null);
-    /*第一个参数弹出显示view  后两个是窗口大小*/
         mPopupWindow = new PopupWindow(sub_view, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-    /*设置背景显示*/
-        mPopupWindow.setBackgroundDrawable(new ColorDrawable(Color.GRAY));
+        mPopupWindow.setBackgroundDrawable(new BitmapDrawable());
+//        mPopupWindow.setBackgroundDrawable(new ColorDrawable(Color.GRAY));
     /*设置触摸外面时消失*/
         mPopupWindow.setOutsideTouchable(true);
     /*设置系统动画*/
@@ -174,4 +165,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         });
 
     }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.btn_menu:
+                showPopupWindow();
+                break;
+        }
+    }
+
+    private void showPopupWindow() {
+        if (!mPopupWindow.isShowing()) {
+            isShow = true;
+            View p = (View) mViewPager.getParent();
+            int[] loc = new int[2];
+            p.getLocationInWindow(loc);
+            mPopupWindow.showAtLocation(p, Gravity.RIGHT | Gravity.TOP, 0, loc[1]);
+        } else {
+            isShow = false;
+        }
+    }
+
 }
